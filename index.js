d3efd140f9f6571364750b55e87cbb023d8c6a69
Vamsi <bvamsi489@gmail.com>
2024-03-01 22:50:52 +0530

    Delete: Unnecessary routes delete

--- index.js
+++ index.js
@@ -5,20 +5,8 @@ require('dotenv').config();
 
 app.use(cors());
 
-app.get('/test', (req, res) => {
-    res.send('<h2>Test</h2>');
-})
-
 app.get('/', (req, res) => {
     res.send('<h2>Home Page</h2>')
-})
-
-app.get('/login', (req, res) => {
-    res.send('<h1>Login page</h1>')
-})
-
-app.get('/signup', (req, res) => {
-    res.send('<h2>Signup page</h2>')
 })
 
 app.listen(process.env.PORT, () => {
